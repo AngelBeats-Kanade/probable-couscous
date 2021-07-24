772fd76a1c99f9ad74b2b2c1c855c6bf55d88d64
AngelBeats-Kanade <58588522+AngelBeats-Kanade@users.noreply.github.com>
2021-07-25 00:26:52 +0800

    Update 2021/7/25

--- uwp-031-answer/MainPage.xaml.cs
+++ uwp-031-answer/MainPage.xaml.cs
@@ -25,19 +25,42 @@ namespace UWP开发入门
             this.InitializeComponent();
         }
 
+        private readonly List<(string Name, Type Page)> _pages = new List<(string Name, Type Page)>
+        {
+            ("Donuts", typeof(DonutPage)),
+            ("Coffee", typeof(CoffeePage)),
+            ("Schedule",typeof(SchedulePage)),
+            ("Complete",typeof(CompletePage)),
+        };
+
         private void MyNavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-
+            if(args.InvokedItemContainer != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Name.Equals(args.InvokedItem.ToString()));
+                MyFrame.Navigate(item.Page);
+            }
         }
 
         private void MyFrame_Navigated(object sender, NavigationEventArgs e)
         {
-
+            if(MyFrame.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+                MyNavigationView.SelectedItem = MyNavigationView.MenuItems
+                                                    .OfType<NavigationViewItem>()
+                                                        .First(n => n.Name.Equals(item.Name));
+            }
         }
 
         private void MyFrame_Loaded(object sender, RoutedEventArgs e)
         {
+            MyFrame.Navigate(typeof(DonutPage));
+        }
 
+        private void MyFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load page:" + e.SourcePageType.FullName);
         }
     }
 }
